1d9f0a0b242b6a51bcb3b08bfd23423b048e7c11
juliang <juliang@melissadata.com>
2019-05-16 14:20:42 -0700

    Readded new favicon and now added mock iss locations.

--- src/app/services/iss-current-location.service.ts
+++ src/app/services/iss-current-location.service.ts
@@ -15,3 +15,11 @@ export class IssCurrentLocationService {
     return this.http.get<IssCurrentLocation>(this.serviceUrl);
   }
 }
+
+export const MOCK_ISS_LOCATIONS = [
+  { message: 'success', timestamp: 1558041176, iss_position: {latitude: '49.2990', longitude: '77.1637'}},
+  { message: 'success', timestamp: 1558041285, iss_position: {latitude: '46.6979', longitude: '86.5573'}},
+  { message: 'success', timestamp: 1558041327, iss_position: {latitude: '45.4836', longitude: '89.9036'}},
+  { message: 'success', timestamp: 1558041386, iss_position: {latitude: '43.6380', longitude: '94.2705'}},
+  { message: 'success', timestamp: 1558041408, iss_position: {latitude: '42.8740', longitude: '95.8864'}},
+];
