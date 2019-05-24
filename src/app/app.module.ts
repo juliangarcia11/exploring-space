d857f88b1da462746e32c75c6830e388c20a8c71
Julian Garcia <jlmurraygarcia@gmail.com>
2019-05-23 18:47:56 -0700

    [Home + PageNotFound] Created PageNotFoundComponent and hooked it into the router so that if a user navigates to an unknown page, they are shown a silly alien and an instructional message. Created a reusable HTML template for PageNotFound and modified the HomeComponent to utilize it as well.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 // Components
-import { RouterSidebarComponent } from './components/ui-elements/router-sidebar/router-sidebar.component';
+import { RouterSidebarComponent } from './components/_shared/router-sidebar/router-sidebar.component';
 import { PeopleInSpaceComponent } from './components/people-in-space/people-in-space.component';
 import { ApodComponent } from './components/apod/apod.component';
 import { IssCurrentLocationComponent } from './components/iss-current-location/iss-current-location.component';
@@ -25,7 +25,8 @@ import { IssCurrentLocationComponent } from './components/iss-current-location/i
 import { IssCurrentLocationService, PeopleInSpaceService } from './services';
 import { HomeComponent } from './components/home/home.component';
 import { FormsModule } from '@angular/forms';
-import { ButtonGroupComponent } from './components/ui-elements/button-group/button-group.component';
+import { ButtonGroupComponent } from './components/_shared/button-group/button-group.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,8 @@ import { ButtonGroupComponent } from './components/ui-elements/button-group/butt
     ApodComponent,
     IssCurrentLocationComponent,
     HomeComponent,
-    ButtonGroupComponent
+    ButtonGroupComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+/**
+ * The main landing page for the app. Displays a header, message,
+ * and image in a nebular card element
+ * Url: <url> || <url>/home
+ */
+
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
+  templateUrl: '../_shared/simple-image-card.html',
   styleUrls: ['./home.component.sass']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  // fields to be used by the template
+  private header = 'Welcome to Outer Space';
+  private imagePath = '../../../assets/green_alien_ahh_200px.png';
+  private message: string;
 
   ngOnInit() {
+    this.message = 'Gathered here is a collection of outer space themed data. ';
+    this.message += 'Please use the navigation sidebar to select a page.';
   }
-
 }
